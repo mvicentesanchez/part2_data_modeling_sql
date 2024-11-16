6315ed8b7725c1cd11acc8ebf66d19ccb86d405b
Marta Vicente Sanchez <m.vicente.sanchez@gmail.com>
2024-11-16 17:24:46 +0100

    Delete scripts directory

--- scripts/table/hubproduct.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE hubproduct (
-    id_product INT PRIMARY KEY,
-    hashkeyproduct CHAR(32) NOT NULL UNIQUE,
-    dt_load_date TIMESTAMP NOT NULL,
-    recordsource VARCHAR(50) NOT NULL
-);
