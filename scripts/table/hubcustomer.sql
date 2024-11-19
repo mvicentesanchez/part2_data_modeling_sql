0ed805dd447c7d9cd773101bbe888b8ec9f91021
Marta Vicente Sanchez <m.vicente.sanchez@gmail.com>
2024-11-20 00:39:39 +0100

    Delete scripts directory

--- scripts/table/hubcustomer.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE hubcustomer (
-    id_customer INT PRIMARY KEY,                  -- Identificador único para cada cliente
-    hashkeycustomer CHAR(32) NOT NULL UNIQUE,     -- Clave hash única para identificar al cliente
-    dt_load_date TIMESTAMP NOT NULL,              -- Fecha y hora de carga del registro
-    recordsource VARCHAR(50) NOT NULL             -- Fuente del registro
-);
--- scripts/test/test_hubcustomer.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO hubcustomer (id_customer, hashkeycustomer, dt_load_date, recordsource)
-VALUES
-    (1, 'A1B2C3D4E5F6G7H8I9J0K1L2M3N4O5P6', '2023-10-01 10:00:00', 'SourceA'),
-    (2, 'P6O5N4M3L2K1J0I9H8G7F6E5D4C3B2A1', '2023-10-02 11:30:00', 'SourceB');
